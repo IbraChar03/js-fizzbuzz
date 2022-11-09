416a5707af34f7f889e2e8b44aac1e0f1dd90bb1
Ibrahim <ibrachar68@gmail.com>
2022-11-09 15:50:02 +0100

    fine esercizio

--- main.js
+++ main.js
@@ -4,19 +4,19 @@ for(let i=1;i<=100;i++){
         const element= `<div class="box box${i}">FizzBuzz</div>`;
         contenitore.innerHTML+=element; 
         let color=document.querySelector(`.box${i}`);
-        color.style.backgroundColor = "#f0466f"
+        color.style.backgroundColor = "#f0466f";
         }
     else if ((i % 5) == 0){
         const element= `<div class="box box${i}">Buzz</div>`;
         contenitore.innerHTML+=element;
         let color=document.querySelector(`.box${i}`);
-        color.style.backgroundColor = "#ffd166"
+        color.style.backgroundColor = "#ffd166";
     }
     else if((i % 3) == 0){
         const element= `<div class="box box${i}">Fizz</div>`;
         contenitore.innerHTML+=element;
         let color=document.querySelector(`.box${i}`);
-        color.style.backgroundColor = "#0cd4a0"
+        color.style.backgroundColor = "#0cd4a0";
    
        }
     else{
